c2dd1408e9091b06589fed27a5f1d5bdfc8815f6
Scott Gordon <me@scottyg.net>
2023-08-21 15:23:49 -0700

    Jest Fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "commonjs": true,
     "es2021": true,
-    "node": true
+    "node": true,
+    "jest": true
   },
   "extends": "eslint:recommended",
   "overrides": [
